6530e827c5c7d3f735ed9c5f1b656d5818236e63
Carlos Bogarin <carlos.bogarin@ucr.ac.cr>
2016-05-24 16:13:04 -0600

    Test.

--- src/Game/Test.java
+++ src/Game/Test.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Game.Assets;
+package Game;
 
 /**
  *
  * @author boga
  */
-public class Assets
+public class Test
 {
 
 }
